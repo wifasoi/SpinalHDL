6c4795bad377c7e4e8cc552e51d279bc59073604
Luca Cristaldi <wifasoi@gmail.com>
2018-06-05 13:44:12 +0200

    Add the sequencer

--- lib/src/main/scala/spinal/lib/sim/bus/wishbone/WishboneSequencer.scala
+++ lib/src/main/scala/spinal/lib/sim/bus/wishbone/WishboneSequencer.scala
@@ -1,4 +1,6 @@
 package spinal.lib.wishbone.sim
+import spinal.lib.uvm.sim._
+
 
 import spinal.sim._
 import spinal.core._
@@ -16,10 +18,16 @@ case class AddressRange(base : BigInt, size: Int){
 }
 
 object WishboneTransaction{
-  implicit def singleToCycle(transaction : WishboneTransaction): Seq[WishboneTransaction] = List(transaction)
+  // implicit def singleToCycle(transaction : WishboneTransaction): WishboneCycle = new WishboneCycle(List(transaction))
+  implicit def singleToList(transaction : WishboneTransaction): Seq[WishboneTransaction] = Seq(transaction)
+  //implicit def singleToCycle(transaction : WishboneTransaction): WishboneCycle = WishboneCycle(List(transaction))
+  // implicit def singleToCycle(transaction : WishboneTransaction): WishboneCycle = List(transaction).asInstanceOf[WishboneCycle]
+  //implicit def singleToCycle(transaction : WishboneTransaction): WishboneCycle = WishboneCycle(List(transaction))
+  //implicit def listToCycle(transactions: Seq[WishboneTransaction]) : WishboneCycle = new WishboneCycle(transactions)
+  //implicit def cycleToList(cycle: WishboneCycle) : Seq[WishboneTransaction] = cycle.cycle
 
   def sampleAsMaster(bus: Wishbone): WishboneTransaction = {
-    val transaction = WishboneTransaction(bus.ADR.toBigInt, bus.DAT_MISO.toBigInt)
+    val transaction = WishboneTransaction(bus.ADR.toBigInt, bus.DAT_MISO.toBigInt, bus.WE.toBoolean)
     if(bus.config.useTGA) transaction.copy(tga = bus.TGA.toBigInt)
     if(bus.config.useTGC) transaction.copy(tga = bus.TGC.toBigInt)
     if(bus.config.useTGD) transaction.copy(tga = bus.TGD_MISO.toBigInt)
@@ -27,7 +35,7 @@ object WishboneTransaction{
   }
 
   def sampleAsSlave(bus: Wishbone): WishboneTransaction = {
-    val transaction = WishboneTransaction(bus.ADR.toBigInt, bus.DAT_MOSI.toBigInt)
+    val transaction = WishboneTransaction(bus.ADR.toBigInt, bus.DAT_MOSI.toBigInt, bus.WE.toBoolean)
     if(bus.config.useTGA) transaction.copy(tga = bus.TGA.toBigInt)
     if(bus.config.useTGC) transaction.copy(tgc = bus.TGC.toBigInt)
     if(bus.config.useTGD) transaction.copy(tgd = bus.TGD_MOSI.toBigInt)
@@ -37,15 +45,17 @@ object WishboneTransaction{
 
 case class WishboneTransaction( address : BigInt = 0,
                                 data : BigInt = 0,
+                                we : Boolean = false,
                                 tga : BigInt  = 0,
                                 tgc : BigInt  = 0,
-                                tgd : BigInt  = 0){
+                                tgd : BigInt  = 0) extends Transaction{
   //override def toString : String = "Address\tData\tTGA\tTGC\tTGD\n%d\t%d\t%d\t%d\t%d".format(address,data,tga,tgc,tgd)
   def masked(mask : BigInt) : WishboneTransaction = this.copy(address = this.address & mask)
 
   def driveAsMaster(bus: Wishbone): Unit = {
     bus.ADR       #= address
     bus.DAT_MOSI  #= data
+    bus.WE #= we
     if(bus.config.useTGA) bus.TGA       #= tga
     if(bus.config.useTGC) bus.TGC       #= tgc
     if(bus.config.useTGD) bus.TGD_MOSI  #= tgd
@@ -64,25 +74,61 @@ case class WishboneTransaction( address : BigInt = 0,
   def randomizeTGA(max : Int, min : Int = 0) : WishboneTransaction = this.copy(tga = Random.nextInt(max - min) + min)
   def randomizeTGC(max : Int, min : Int = 0) : WishboneTransaction = this.copy(tgc = Random.nextInt(max - min) + min)
   def randomizeTGD(max : Int, min : Int = 0) : WishboneTransaction = this.copy(tgd = Random.nextInt(max - min) + min)
+
+  override def toString: String = List(address,data,we,tga,tgc,tgd).mkString("\t")
 }
 
+// object WishboneCycle{
+//   implicit def listToCycle(list: Seq[WishboneTransaction]) : WishboneCycle = new WishboneCycle(list)
+//   implicit def cycleToList(cycle: WishboneCycle) : Seq[WishboneTransaction] = cycle.cycle
+// }
+case class WishboneCycle(val transactions: Seq[WishboneTransaction]) extends Transaction{
+  //override def equals(that: Any) : Boolean = true
+  override def toString: String = {
+    List("address","data","we","tga","tgc","tgd").mkString("\t") + "\n" +
+    transactions.mkString("\n")
+  }
+}
+
+//object WishboneCycle{
+//    implicit def listToCycle(list: Seq[WishboneTransaction]) : WishboneCycle = new WishboneCycle(list)
+//  implicit def cycleToList(cycle: WishboneCycle) : Seq[WishboneTransaction] = cycle.cycle
+//}
+
+
+// object WishboneCycle{
+//   //implicit def listToCycle(list: immutable.Seq[WishboneTransaction]) : Cycle[WishboneTransaction] = classOf[Cycle[WishboneTransaction]].getConstructor(classOf[Cycle[WishboneTransaction]]).newInstance(list)
+//   implicit def listToCycle(list: immutable.Seq[WishboneTransaction]) : WishboneCycle = new WishboneCycle(list)
+//   implicit def cycleToList(cycle: WishboneCycle) : immutable.Seq[WishboneTransaction] = cycle.transactions
+// }
+// class WishboneCycle(val transactions : immutable.Seq[WishboneTransaction]) extends Cycle[WishboneTransaction]
+
+
+// object WishboneSequencer{
+//   //def apply(builder: () => Seq[WishboneTransaction]) = new WishboneSequencer(builder)
+//   def apply(builder: => Seq[WishboneTransaction]) = new WishboneSequencer(builder)
+//   //implicit def singleTransactionToList(transaction : WishboneTransaction): Seq[WishboneTransaction] = Seq(transaction)
+
+// }
+
+// class WishboneSequencer(builder: => Seq[WishboneTransaction]){
+//   val transactions = new scala.collection.mutable.Queue[Seq[WishboneTransaction]]()
+//   //val builder:() => WishboneTransaction
+
+
+//   def nextTransaction: Seq[WishboneTransaction] = transactions.dequeue()
+//   def addTransaction(transaction: Seq[WishboneTransaction]): Unit = transactions.enqueue(transaction)
+
+//   //def transactionBuilder(builder: () => WishboneTransaction): Unit = this.builder = builder
+//   def generateTransactions(number: Int = 1): Unit = for(i <- 0 to number) addTransaction(builder)
+
+//   def isEmpty: Boolean = transactions.isEmpty
+// }
 object WishboneSequencer{
-  //def apply(builder: () => Seq[WishboneTransaction]) = new WishboneSequencer(builder)
-  def apply(builder: => Seq[WishboneTransaction]) = new WishboneSequencer(builder)
-  //implicit def singleTransactionToList(transaction : WishboneTransaction): Seq[WishboneTransaction] = Seq(transaction)
-
+  def apply(callback: (WishboneCycle) => Unit@suspendable): WishboneSequencer ={
+    val sequencer = new WishboneSequencer()
+    sequencer.addCallback(callback)
+    sequencer
+  }
 }
-
-class WishboneSequencer(builder: => Seq[WishboneTransaction]){
-  val transactions = new scala.collection.mutable.Queue[Seq[WishboneTransaction]]()
-  //val builder:() => WishboneTransaction
-
-
-  def nextTransaction: Seq[WishboneTransaction] = transactions.dequeue()
-  def addTransaction(transaction: Seq[WishboneTransaction]): Unit = transactions.enqueue(transaction)
-
-  //def transactionBuilder(builder: () => WishboneTransaction): Unit = this.builder = builder
-  def generateTransactions(number: Int = 1): Unit = for(i <- 0 to number) addTransaction(builder)
-
-  def isEmpty: Boolean = transactions.isEmpty
-}
+class WishboneSequencer extends Sequencer[WishboneCycle]
